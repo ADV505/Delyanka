4d945ed85af82554d06d5d9508d2844f5ac6d486
ADV505 <dmitriy-505@mail.ru>
2024-06-29 11:58:55 +0500

    Добавьте файлы проекта.

---
+++ Report/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Report.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Report/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Report.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
